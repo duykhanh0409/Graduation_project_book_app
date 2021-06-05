b7c68211d26abdb3544aa31eb663c0ad0be63131
khanhNguyen <duykhanhlb4999@gmail.com>
2021-06-05 22:13:53 +0700

    add multip  image picker

--- lib/screens/host/widget/photo_room.dart
+++ lib/screens/host/widget/photo_room.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:multi_image_picker2/multi_image_picker2.dart';
 
 class PhotoRoom extends StatefulWidget {
   @override
@@ -7,8 +11,92 @@ class PhotoRoom extends StatefulWidget {
 }
 
 class _PhotoRoomState extends State<PhotoRoom> {
+  List<Asset> images = <Asset>[];
+  String _error = 'No Error Dectected';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 300,
+        );
+      }),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = <Asset>[];
+    //String error = 'No Error Detected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(
+          takePhotoIcon: "chat",
+          doneButtonTitle: "Fatto",
+        ),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Image'));
+    print(images.length > 0 ? images[1] : images);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange[900],
+          title: const Text('Photo Room'),
+        ),
+        body: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Pick images"),
+              onPressed: loadAssets,
+            ),
+            Expanded(
+              child: buildGridView(),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Continues',
+          child: Icon(
+            Icons.arrow_right_alt_sharp,
+            size: 30,
+          ),
+        ),
+      ),
+    );
   }
 }
